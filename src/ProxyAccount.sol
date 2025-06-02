9697798f2c070d897d9716bf605b524df0a8f3c2
oyaboon <floome@outlook.com>
2025-06-02 17:14:38 +0800

    feat: add vesting contract with cliff

---
+++ src/ProxyAccount.sol
@@ -0,0 +1,47 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+/**
+ * @title ProxyAccount
+ * @dev A contract that allows the owner to execute strategies and transfer tokens
+ */
+contract ProxyAccount {
+    address public owner;
+
+    /**
+     * @dev Sets the owner of the contract
+     * @param _owner The address that will own this contract
+     */
+    constructor(address _owner) {
+        owner = _owner;
+    }
+
+    /**
+     * @dev Modifier to restrict access to owner only
+     */
+    modifier onlyOwner() {
+        require(msg.sender == owner, "ProxyAccount: caller is not the owner");
+        _;
+    }
+
+    /**
+     * @dev Executes a strategy by calling an external contract
+     * @param strategy The address of the strategy contract to call
+     * @param data The calldata to send to the strategy contract
+     */
+    function executeStrategy(address strategy, bytes calldata data) external onlyOwner {
+        (bool success, ) = strategy.call(data);
+        require(success, "ProxyAccount: strategy execution failed");
+    }
+
+    /**
+     * @dev Transfers ERC20 tokens to the owner
+     * @param token The address of the ERC20 token contract
+     * @param amount The amount of tokens to transfer
+     */
+    function transferToken(address token, uint256 amount) external onlyOwner {
+        IERC20(token).transfer(owner, amount);
+    }
+} 
